0bcf85f6681471e69da83e7ec8e625b2884c5c3d
Nitsuga28k <l21240045@smartin.tecnm.mx>
2024-10-10 10:37:30 -0600

    cambio de clase

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ if (!empty($_SESSION['activa'])) {
             </div>
             <div class="col">
                 <div class="auth-form">
-                    <h1>AUTENTICACIÓN TOS</h1>
+                    <h1>AUTENTICACIÓN </h1>
 
                     <form method="POST">
                         <div>
